24f6fe659cfc7e64d957ef34371dcc9629a87fb1
Sreekar Swarnapuri <sreekar.swarnapuri@hotmail.com>
2020-09-15 23:49:09 +0530

    ArrayManipulation coding challenge from Arrays under Hacker Rank

--- src/main/java/com/goldenstudios/codingchallenges/hackerrank/arrays/hard/ArrayManipulation.java
+++ src/main/java/com/goldenstudios/codingchallenges/hackerrank/arrays/hard/ArrayManipulation.java
@@ -1,7 +1,68 @@
 package com.goldenstudios.codingchallenges.hackerrank.arrays.hard;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class ArrayManipulation {
 
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] queries) {
 
+        long a[] = new long[n+1];
+        long sum=0,max=0;
+
+        for(int i=0; i<queries.length; i++) {
+            a[queries[i][0]-1]+=queries[i][2];
+            a[queries[i][1]]-=queries[i][2];
+        }
+
+        for (long x:
+             a) {
+            sum+=x;
+            if(max<sum)
+                max =sum;
+        }
+
+        return max;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        int[][] queries = new int[m][3];
+
+        for (int i = 0; i < m; i++) {
+            String[] queriesRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 3; j++) {
+                int queriesItem = Integer.parseInt(queriesRowItems[j]);
+                queries[i][j] = queriesItem;
+            }
+        }
+
+        long result = arrayManipulation(n, queries);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 
 }
